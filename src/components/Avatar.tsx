c998250270ea3e46c0a0c2cfb9cf75966d9e1eb5
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-07 12:49:22 -0300

    TypeScript no React

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,17 @@
 import styles from './Avatar.module.css';
 
-export function Avatar({hasBorder = true, src}){
-    return(
-    <img
-        className={hasBorder ? styles.avatarWithBorder : styles.Avatar}
-        src={src}
-    />
-);
+interface AvatarProps {
+    hasBorder?: boolean;
+    src: string;
+    alt?: string;
+}
+
+export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+    return (
+        <img
+            className={hasBorder ? styles.avatarWithBorder : styles.Avatar}
+            src={src}
+            alt={alt}
+        />
+    );
 }
